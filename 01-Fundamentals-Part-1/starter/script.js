4313f60c937c8da2ffe5dcc313d982232307e8bf
Victoria Fox-Collis <victoriafoxc@gmail.com>
2023-08-17 15:43:39 -0600

    comment out solution

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,40 +1,40 @@
-let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
+// let js = "amazing";
+// if (js === "amazing") alert("Javascript is FUN!");
 
-let country = 'Portugal';
-let continent = 'Europe';
-let population = 10;
-console.log(country);
-console.log(continent);
-console.log(population);
+// let country = 'Portugal';
+// let continent = 'Europe';
+// let population = 10;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-let isIsland = false;
- let language;
- console.log(typeof isIsland);
- console.log(typeof population);
- console.log(typeof country);
- console.log(typeof language);
+// let isIsland = false;
+//  let language;
+//  console.log(typeof isIsland);
+//  console.log(typeof population);
+//  console.log(typeof country);
+//  console.log(typeof language);
 
- language = 'portuguese';
-const country = 'Portugal';
-const continent = 'Europe';
-const isIsland = false;
-isIsland = true;
+//  language = 'portuguese';
+// const country = 'Portugal';
+// const continent = 'Europe';
+// const isIsland = false;
+// isIsland = true;
 
-console.log(population / 2);
-population++;
-console.log(population);
-console.log(population > 6);
-console.log(population < 33);
-const description1 =
-country +
-' is in ' +
-continent +
-', and its ' +
-population +
-' million people speak ' +
-language;
-console.log(description1);
+// console.log(population / 2);
+// population++;
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
+// const description1 =
+// country +
+// ' is in ' +
+// continent +
+// ', and its ' +
+// population +
+// ' million people speak ' +
+// language;
+// console.log(description1);
 
-const description = `${country} is in ${continent}, and its
- ${population} million people speak ${language}`;
+// const description = `${country} is in ${continent}, and its
+//  ${population} million people speak ${language}`;
